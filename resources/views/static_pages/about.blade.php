fef98fe4003e624c5d58a7f6c6d5eb7d3d49643c
yaojinhong <yaojinhong@jiuchunjiaoyu.com>
2017-04-21 11:46:10 +0800

    Initial commit

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','关于页')
+@section('content')    
+    <h1> guan于夜夜夜夜 </h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','召唤师峡谷')
+@section('content')    
+    <h1> 主页 </h1>
+@stop
